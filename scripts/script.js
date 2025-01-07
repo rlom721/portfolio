0ce19c2102246e7165fffd8cd1c0ef0e2458b51e
Rafey <59098398+rlom721@users.noreply.github.com>
2025-01-07 13:04:46 -0800

    Add transitions to hero

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+var email = "rafaela.lomboy@gmail.com";
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -9,8 +11,18 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 function copyText() {
-    var copyText = "rafaela.lomboy@gmail.com";
+    var text = email;
 
-    navigator.clipboard.writeText(copyText);
-    alert("Copied to clipboard: " + copyText);
+    navigator.clipboard.writeText(text);
+    alert("Copied to clipboard: " + text);
 }
+  
+let element = document.getElementById("fadeIn");
+let opacity = 0;
+let fadeIn = setInterval(() => {
+   if (opacity >= 1) {
+      clearInterval(fadeIn);
+   }
+   element.style.opacity = opacity;
+   opacity += 0.01;
+}, 10);
